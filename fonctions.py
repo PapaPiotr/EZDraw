6f6a4da225dd47795b7e00f0b6e6537b8433c570
PapaPiotr <167995662+PapaPiotr@users.noreply.github.com>
2024-04-29 16:28:29 -0300

    Update temp directory path

--- fonctions.py
+++ fonctions.py
@@ -118,7 +118,7 @@ def submit(self, info):
 
 # draws the page
     page = draw_page(orient, t_state, title, n_state, num, col, margin, boxes)
-    temp_jpg = os.path.join(".temp","temp.jpg")
+    temp_jpg = os.path.join("temp","temp.jpg")
     page.save(temp_jpg)
     return(page)
 
